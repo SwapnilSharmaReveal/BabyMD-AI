2f1cf51cc600c87612c5eabb123a3eae2d7427d4
vinubhat092 <vinayakbhat94@gmail.com>
2023-11-17 11:34:32 +0530

    base prompt updated

--- llama_replicate.py
+++ llama_replicate.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import replicate
 import os
+import re
 
 # App title
 st.set_page_config(page_title="BabyMD AI Assistant", initial_sidebar_state="collapsed")
@@ -81,22 +82,36 @@ def clear_chat_history():
     st.session_state.messages = [{"role": "assistant", "content": "Hey! Welcome to BabyMD - a revolutionary Indian Pediatric Care Chain offering best-in-class, round the clock clinical care. I am X, your personal assistant to help you get instant consultation with our expert pediatricians. What can I help you today?"}]
 st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
 
+def generate_llama2_summary2(texts):
+   text = ' '.join(texts.split())
+   #text = """User: my baby is not feeling well. Assistant: Sorry to hear that. Can you please tell me a little bit more about your baby's symptoms? For example, what is their age and gender? User: age is 4 year old, gender is male. Assistant: Great, that helps me to narrow down the possible causes. Can you please tell me what their body temperature is right now? User: current temperature is 112f."""
+   summ = f"<s>\
+        [SYS]Your task is to summarize the {text} to give the health ovierview and all the information given by user.[/SYS]\
+        [INT]Remember to provide a descriptive summary having all the information in the paragraph format.[/INT]\
+        [INT]After that from a new line, list down the following parameters from the summary : 1.Chief Compliant, 2.Severity, 3.Duration, 4.Age, 5.Birth History, 6.Feeding History, 7.Social History, 8.Medical History, 9.Related symptoms[/INT]\
+        </s>"
+   output = replicate.run('a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5',
+                            input={"prompt": f"{summ}",
+                                    "temperature":temperature, "top_p":top_p, "max_length":max_length, "repetition_penalty":1})
+   return output
+
 # Function for generating LLaMA2 response. Refactored from https://github.com/a16z-infra/llama2-chatbot
 def generate_llama2_response(prompt_input):
     string_dialogue = "<s>\
         As an [SYS], your role is to conduct a conversation with concern and in a professional manner while collecting information about a child's symptoms and basic information from their parents. Your objective is to gather the necessary information within 9 questions while avoiding any potential diagnosis or cause. Please handle any sensitive information shared by the parents with caution and ensure encryption for privacy and security. Inquire about the specific symptoms the child is experiencing and mandatorily collect the following information within two or three lines each: 1. Age and gender of the child, 2. Basic health information, 3. Chief complaint and duration, 4. Allergies, 5. Feeding history, 6. Existing medical conditions, 7. Medications being taken, 8. Birth history, 9. Recent social activities. Once you have gathered all the necessary information, conclude with the message, 'Thank you for sharing the details. Our pediatrician will respond within the next 10 minutes. Remember not to answer any questions that are unrelated to the medical context.\
-        [INT]You need to reply for the users {input}[/INT]\
             "
     conversations = ''
     for dict_message in st.session_state.messages:
         if dict_message["role"] == "user":
             string_dialogue += "User: " + dict_message["content"] + "\n\n"
+            conversations += "User: " + dict_message["content"] + "\n\n"
         else:
             string_dialogue += "Assistant: " + dict_message["content"] + "\n\n"
+            conversations += "Assistant: " + dict_message["content"] + "\n\n"
     output = replicate.run('a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5', 
                            input={"prompt": f"{string_dialogue} {prompt_input} Assistant: ",
                                   "temperature":temperature, "top_p":top_p, "max_length":max_length, "repetition_penalty":1})
-    return output
+    return output,conversations
 
 # User-provided prompt
 if prompt := st.chat_input(disabled=not replicate_api):
@@ -108,12 +123,23 @@ if prompt := st.chat_input(disabled=not replicate_api):
 if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
-            response = generate_llama2_response(prompt)
+            response,conversations = generate_llama2_response(prompt)
             placeholder = st.empty()
             full_response = ''
             for item in response:
                 full_response += item
                 placeholder.markdown(full_response)
             placeholder.markdown(full_response)
+            word_to_search_1 = "10 minutes"
+            word_to_search_2 = "10 mins"
+            match1 = re.search(r'\b' + re.escape(word_to_search_1) + r'\b', full_response)
+            match2 = re.search(r'\b' + re.escape(word_to_search_2) + r'\b', full_response)
+            full_summary = ''
+            if (match1 or match2):
+                st.markdown("Below is the summary:")
+                response1 = generate_llama2_summary2(conversations)
+                for item in response1:
+                    full_summary += item
+                st.markdown(full_summary)
     message = {"role": "assistant", "content": full_response}
     st.session_state.messages.append(message)
